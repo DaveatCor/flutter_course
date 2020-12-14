56c85ba9e594b3137931174e6626ad000dec674f
DaveatCor <condaveat@gmail.com>
2020-12-14 09:23:05 +0700

    8. Declaring The Tickerprovider

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_flutter/screens/widets/cat.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,5 +32,7 @@ class _HomeState extends State<Home> {
     );
   }
   
-  Widget animationBuilder(){}
+  Widget animationBuilder(){
+    return Cat();
+  }
 }
