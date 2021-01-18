44410c3db83608f4a31ae1015749a0574ed7c77c
DaveatCor <condaveat@gmail.com>
2021-01-18 11:10:27 +0700

    25. Adjusting

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this
     );
 
