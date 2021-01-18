dfe6786d736a9967b6f968d25fcbda3fd0844c08
DaveatCor <condaveat@gmail.com>
2021-01-18 11:42:03 +0700

    27

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -50,6 +50,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
             children: [
               buildCatAnimation(),
               buildBox(),
+              buildFlapLeft()
             ],
           ),
         )
