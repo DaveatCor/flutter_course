6c615fb0d6f567211b807b31f645c4fcb42035ed
DaveatCor <condaveat@gmail.com>
2021-03-01 14:49:48 +0700

    writing Expectaions

--- test/src/news_api_provider_test.dart
+++ test/src/news_api_provider_test.dart
@@ -2,3 +2,13 @@ import 'package:tutorial_flutter/src/resource/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';  
+
+void main(){
+  test("FetchTopIds return a list", (){
+    //setup test case
+    final sum = 1+3;
+
+    // Expectation
+    expect(sum, 4);
+  });
+}
