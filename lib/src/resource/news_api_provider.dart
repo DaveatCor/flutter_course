a3b488ac630a07b48dfb25c09d6e11dbb97a60a9
DaveatCor <condaveat@gmail.com>
2021-03-03 11:57:29 +0700

    finish lecture 19, 5. Returning JSON, 6, Testing FetchItem, date 03/Mar/21

--- lib/src/resource/news_api_provider.dart
+++ lib/src/resource/news_api_provider.dart
@@ -4,17 +4,17 @@ import 'package:tutorial_flutter/src/model/items_m.dart';
 
 class NewsApiProvider{
 
-  Client _client = Client();
+  Client client = Client();
 
   final String _root = 'https://hacker-news.firebaseio.com';
 
   fetchTopIds() async {
-    final response = await _client.get('$_root/v0/topstories.json?print=pretty');
-    final ids = json.decode(response.body);
+    final response = await client.get('$_root/v0/topstories.json?print=pretty');
+    return json.decode(response.body);
   }
 
   fetchItem(int id) async {
-    final response = await _client.get('$_root/v0/item/$id.json');
+    final response = await client.get('$_root/v0/item/$id.json');
     final parsedJson = json.decode(response.body);
 
     return ItemModel.fromJson(parsedJson);
--- test/src/news_api_provider_test.dart
+++ test/src/news_api_provider_test.dart
@@ -2,13 +2,29 @@ import 'package:tutorial_flutter/src/resource/news_api_provider.dart';
 import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';  
+import 'package:http/testing.dart';
 
 void main(){
-  test("FetchTopIds return a list", (){
-    //setup test case
-    final sum = 1+3;
+  test("FetchTopIds return a list", () async {
+    final newsApi = NewsApiProvider();
 
-    // Expectation
-    expect(sum, 4);
+    // Override client object with MockClient Fake Response Data
+    newsApi.client = MockClient((request) async {
+      // Fake Data For Response
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final data = await newsApi.fetchTopIds();
+    expect(data, [1, 2, 3, 4]);  
+  });
+
+  test("FetchItem return ID", () async {
+    final newApis = NewsApiProvider();
+    newApis.client = MockClient((request) async {
+      return Response(json.encode({'id': 123}), 200);
+    });
+
+    final item = await newApis.fetchItem(123);
+    expect(item.id, 123);
   });
 }
