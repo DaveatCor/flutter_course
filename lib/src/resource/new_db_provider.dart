7aa8ce161af9b85eeb92e584fa2e1ec8485ee2ec
DaveatCor <condaveat@gmail.com>
2021-03-05 11:18:51 +0700

    add file new db provider.dart, date 05/Mar/21

---
+++ lib/src/resource/new_db_provider.dart
@@ -0,0 +1,6 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:tutorial_flutter/src/model/items_m.dart';
