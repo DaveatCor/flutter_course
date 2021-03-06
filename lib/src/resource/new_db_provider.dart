80a6fdb05467f70aa41b72a0ca01f19e7eb06e69
DaveatCor <condaveat@gmail.com>
2021-03-06 10:44:57 +0700

    3. Async constructor, 4. Creating a Db connecting, date 06/Mar/21

--- lib/src/resource/new_db_provider.dart
+++ lib/src/resource/new_db_provider.dart
@@ -4,3 +4,19 @@ import 'dart:io';
 import 'dart:async';
 import 'package:path/path.dart';
 import 'package:tutorial_flutter/src/model/items_m.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "items.db");
+    db = await openDatabase(
+      path, 
+      version: 1,
+      onCreate: (Database newDb, int version){
+        
+      }
+    )
+  }
+}
