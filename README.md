3bfeda490dcf3b44283f34a8f9532295edcef7c5
DaveatCor <condaveat@gmail.com>
2021-03-08 11:13:43 +0700

    5.Create table with sqlite, 6. Adding table Column, 7. Multiple line, 8.issuing query, date 08/Mar/21

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 1. Testing with dart
 2. A Few Import
+
+6. Issuing query
+    - db.query(
+        "Items",
+        columns: null,
+        where: "id = ?", => ? refer to whereArgs list
+        whereArgs: [id]
+    )
--- lib/src/resource/new_db_provider.dart
+++ lib/src/resource/new_db_provider.dart
@@ -8,6 +8,8 @@ import 'package:tutorial_flutter/src/model/items_m.dart';
 class NewsDbProvider {
   Database db;
 
+  //BLOB is a big set of data. For use inside SQlite String
+
   init() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentDirectory.path, "items.db");
@@ -15,8 +17,36 @@ class NewsDbProvider {
       path, 
       version: 1,
       onCreate: (Database newDb, int version){
-        
+        newDb.execute("""
+          CREATE TABLE Items(
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
       }
-    )
+    );
+  }
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id]
+    );
+
+    if (maps.length > 0){
+
+    }
+    return null;
   }
 }
