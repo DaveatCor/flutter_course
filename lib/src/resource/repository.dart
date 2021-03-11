a2493e4b5ae6ef56e6c13f79932d660ab0a5f715
DaveatCor <condaveat@gmail.com>
2021-03-11 12:49:37 +0700

    create repository file, 11. Implementing The Repository,  date 11/Mar/21

---
+++ lib/src/resource/repository.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'new_db_provider.dart';
+import 'news_api_provider.dart';
+import 'package:tutorial_flutter/src/model/items_m.dart';
+
+class Repository{
+  NewsDbProvider dbProvider = NewsDbProvider();
+  NewsApiProvider apiProvider = NewsApiProvider();
+
+  fetchTopIds(){
+
+  }
+
+  fetchItem(){
+    
+  }
+}
